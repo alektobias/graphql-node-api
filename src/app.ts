b03176515177860e1e3e8418e3ece80d930ac56a
Alek Tobias <alektobias@gmail.com>
2020-03-08 11:22:26 -0300

    feat: security config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
+import dotenv from 'dotenv' 
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
+
 import routes from './routes';
 import schema from './graphql/schema';
 class App {
   public server: express.Application;
   constructor() {
+    dotenv.config();
     this.server = express();
 
     this.middlewares();
@@ -13,9 +17,10 @@ class App {
 
   private middlewares(): void {
     this.server.use(express.json());
-
+    this.server.use(cors({origin: process.env.ORIGIN}))
     this.server.use('/graphql', graphqlHTTP({
       schema: schema,
+      graphiql: process.env.NODE_ENV === 'development'
     }));
   }
 
